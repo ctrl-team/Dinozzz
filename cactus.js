8f050078b59e5142638b4aeb3ba9d96f5d495dfb
Nimplex <39964594+Nimplex@users.noreply.github.com>
2019-11-27 19:18:26 +0100

    done

--- cactus.js
+++ cactus.js
@@ -1,14 +1,34 @@
 class cactus {
-  constructor() {
-    this.x = canvas.width + 150;
-    this.y = canvas.height - 130
-    this.width = 150;
-    this.height = 200;
-    this.xspeed = 15;
-    this.count = 0;
+  constructor(type = 0) {
+    // type 0 - cactus ; type 1 - ufo
+    this.type = type;
+    if (this.type == 0) {
+      this.x = canvas.width + 150;
+      this.y = canvas.height - 130;
+      this.width = 120;
+      this.height = 200;
+      this.xspeed = 15;
+      this.count = 0;
+      this.model = new Image();
+      this.model.src = "img/cactus1.png";
+    } else if (this.type == 1) {
+      this.x = canvas.width + 150;
+      this.y = canvas.height - 260;
+      this.width = 120;
+      this.height = 100;
+      this.xspeed = 15;
+      this.count = 0;
+      this.model = new Image();
+      this.model.src = "img/ufo1.png";
+    }
   }
   show() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    //ctx.fillRect(this.x, this.y, this.width, this.height);
+    if (this.type == 1) {
+      ctx.drawImage(this.model, this.x, this.y);
+    } else if (this.type == 0) {
+      ctx.drawImage(this.model, this.x, this.y - 70);
+    }
   }
 
   move() {
